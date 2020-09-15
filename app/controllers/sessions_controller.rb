589819d9fc3015f881c37843347fa56c1c7ff693
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-09-15 19:50:05 +0000

    first commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+end
+def create
+user = User.find_by(email: params[:session][:email].downcase)
+if user && user.authenticate(params[:session][:password])
+  session[:user_id] = user.id
+  flash[:notice] = 'login successful'
+  redirect_to posts_path
+else
+  flash[:danger] = 'failed to login'
+  render :new
+end
+end
+def destroy
+    session.delete(:user_id)
+    flash[:notice ]= 'logged out'
+    redirect_to new_session_path
+  end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  before_validation { email.downcase! }
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+                       
+end
